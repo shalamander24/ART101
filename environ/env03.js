007cc4238bf48a143395bdb178d8f27c30a4d839
ssan117 <sallysan0924@gmail.com>
2025-10-27 21:02:10 -0700

    2

--- environ/env03.js
+++ environ/env03.js
@@ -2,10 +2,18 @@
 let mystarters = ["Fuzzy", "Willow", "Angel"];
 // declaring an object with name myfavriotestarters
 
+function askNumber(whatNumber) {
+let userNumber = prompt("Guess 1-10?");
+if (userNumber == whatNumber) {$("#output").html("The crystals hold the truth of what truely happened that night, you must collect all six...");}
+else {$("#output").html("You are not ready for the truth...");}}
+$("#mystery-button").click(function () {
+    askNumber(6);
+});
+
 let megaSentence;
 
 megaSentence3 = "<p>In a far away land there lives a little dragon names spike, he loved crystals until all the crystals were in his possesion" + ", his best friends are " + mystarters[0] + " and " + mystarters[2] + "."+"</p>";
-megaSentence2 = "<p>Now he rest alone with the jewels as he is rest to slumber for the next century" + "</p>";
+megaSentence2 = "<p>Now he rest alone with the jewels as he is rest to slumber for the next century...or does he?" + "</p>";
 megaSentence= "<h2>This is totally legit</h2>";
 megaSentence4 = "<p>This is Spike, he was a friendly dragon, but with time everyone can lose something</p>";
 
@@ -21,3 +29,4 @@ $("#mypic1").after(megaSentence);
 
 $("#output").html(megaSentence2);
 $("#mypic3").after(megaSentence2);      
+
